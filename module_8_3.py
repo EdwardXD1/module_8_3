6ed9807f22c61028475173a97ef66b1446226968
EdwardXD1 <edwardkulagin@mail.ru>
2025-01-26 18:28:15 +0300

    Add files via upload

--- module_8_3.py
+++ module_8_3.py
@@ -1,52 +1,59 @@
-class Car:
-    def __init__(self, model: str, vin: int, numbers: str):
-        self.model = model 
-        if self.__is_valid_vin(vin):
-            self.__vin = vin
-        if self.__is_valid_numbers(numbers):
-            self.__numbers = numbers
-
-    def __is_valid_vin(self, vin_number):
-        if not isinstance(vin_number, int):
-            raise IncorrectVinNumber('Некорректный тип vin номер')
-        elif len(str(vin_number)) != 7:
-            raise IncorrectVinNumber('Неверный диапазон для vin номера')
-        else:
-            return True
-
-    def __is_valid_numbers(self, numbers):
-        if not isinstance(numbers, str):
-            raise IncorrectCarNumbers('Некорректный тип данных для номеров')
-        elif len(numbers) != 6:
-            raise IncorrectCarNumbers('Неверная длина номера')
-        else:
-            return True
+class IncorrectCarNumbers(Exception):
+    def __init__(self, message, *args):
+        self.message = message
 
 
 class IncorrectVinNumber(Exception):
-    def __init__(self, message):
+    def __init__(self, message, *args):
         self.message = message
 
 
-class IncorrectCarNumbers(Exception):
-    def __init__(self, message):
-        self.message = message
+class Car:
+
+    def __init__(self, model, vin, number):
+        self.model = model
+        if self.__is_valid_vin(vin):
+            self.vin = vin
+        if self.__is_valid_numbers(number):
+            self.numbers = number
+
+    def __is_valid_vin(self, vin_number):
+        if not isinstance(vin_number, int):
+            raise IncorrectVinNumber("Некорректный тип vin номер")
+        elif vin_number not in range(1000000, 9999999):
+            raise IncorrectVinNumber('Неверный диапазон для vin номера')
+        return True
+
+    def __is_valid_numbers(self, number):
+        if not isinstance(number, str):
+            raise IncorrectVinNumber('Некорректный тип данных для номеров')
+        elif len(number) != 6:
+            raise IncorrectCarNumbers('Неверная длина номера')
 
 
-def createCar(model: str, vin: int, numbers: str) -> Car:
-    try:
-        car = Car(model, vin, numbers)
-    except (IncorrectVinNumber, IncorrectCarNumbers) as exc:
-        print(exc.message)
-        return None
-    else:
-        print(f'{car.model} успешно создан')
-        return car
+try:
+  first = Car('Model1', 1000000, 'f123dj')
+except IncorrectVinNumber as exc:
+  print(exc.message)
+except IncorrectCarNumbers as exc:
+  print(exc.message)
+else:
+  print(f'{first.model} успешно создан')
 
+try:
+  second = Car('Model2', 300, 'т001тр')
+except IncorrectVinNumber as exc:
+  print(exc.message)
+except IncorrectCarNumbers as exc:
+  print(exc.message)
+else:
+  print(f'{second.model} успешно создан')
 
-if __name__ == '__main__':
-    first = createCar('Model1', 1000000, 'f123dj')
-    second = createCar('Model2', 300, 'т001тр')
-    third = createCar('Model3', 2020202, 'нет номера')
-    fourth = createCar('Dodge Charger', 666.777, '2JRI424')
-    fifth = createCar('Aston Martin DBS', 8026007, 122222)
+try:
+  third = Car('Model3', 2020202, 'нет номера')
+except IncorrectVinNumber as exc:
+  print(exc.message)
+except IncorrectCarNumbers as exc:
+  print(exc.message)
+else:
+  print(f'{third.model} успешно создан')
